dbce67589aabd700ca45ed368ba4f05f25a16d6c
Rinaldo Belisario Junior <rinaldobjr@gmail.com>
2023-04-14 00:21:26 -0400

    Inicio do Projeto

---
+++ src/main/java/br/com/projeto/crudsimples/CrudsimplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.crudsimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudsimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudsimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/crudsimples/CrudsimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.crudsimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
